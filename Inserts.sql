aeea65978543d60e28c902bb5b55371384b548df
MGMenani <migramenani@gmail.com>
2018-09-17 12:21:52 -0600

    Script de base de datos y pocos inserts

---
+++ Inserts.sql
@@ -0,0 +1,12 @@
+use Boneage
+
+INSERT INTO TipoDeUsuario VALUES('Administrador')
+INSERT INTO TipoDeUsuario VALUES('Médico')
+
+insert into Usuario 
+values(123456789, 'Michael', 'Granados Menani', 2)
+
+INSERT INTO Expediente VALUES(711111111, 'Alonso', 'Rivas Solano', '1995-01-18')
+INSERT INTO Expediente VALUES(111111111, 'Jason', 'Latouche Jiménez', '01-05-1996')
+
+
