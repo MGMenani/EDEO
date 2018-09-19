0cc1ce20f69de7930130c5953421151db5d60a9b
Jason Latouche <96jaslat@gmail.com>
2018-09-19 17:09:41 -0600

    Upload image by Drag&Drop

--- Project EDEO/Project EDEO/Controllers/EstimationsController.cs
+++ Project EDEO/Project EDEO/Controllers/EstimationsController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,18 @@ namespace Project_EDEO.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadFiles(IEnumerable<HttpPostedFileBase> files)
+        {
+            foreach (var file in files)
+            {
+                string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                file.SaveAs(Path.Combine(Server.MapPath("~/Uploads"), filePath));
+                //Here you can write code for save this information in your database if you want
+            }
+            return Json("file uploaded successfully");
         }
     }
 }
