51fb638b3638799857c9fe090f6eb896dcd53e83
Alonso Rivas Solano <alonsors.809@gmail.com>
2018-09-26 15:19:04 -0600

    Nuevo Edit

---
+++ ProjectEDEO/Project EDEO/Migrations/201809262059414_cambio2.cs
@@ -0,0 +1,16 @@
+namespace Project_EDEO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class cambio2 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
