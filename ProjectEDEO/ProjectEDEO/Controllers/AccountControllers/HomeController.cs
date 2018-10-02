ebe12361af02ecbc0ae9cbdc7e454cca8854ccc4
Jason Latouche <96jaslat@gmail.com>
2018-10-01 20:01:15 -0600

    Fixed folder NAME

---
+++ ProjectEDEO/ProjectEDEO/Controllers/AccountControllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_EDEO.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // Check if the user is not logged in
+            if (!User.Identity.IsAuthenticated)
+            {
+                // Redirect to the About landing page
+                return RedirectToAction("Index", "About");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FileUpload(HttpPostedFileBase file)
+        {
+            try
+            {
+                string filePath = Guid.NewGuid() + Path.GetExtension(file.FileName);
+                file.SaveAs(Path.Combine(Server.MapPath("~/images/uploads"), filePath));
+            }
+            catch (Exception exception)
+            {
+                return Json(new
+                {
+                    success = false,
+                    response = exception.Message
+                });
+            }
+
+            return Json(new
+            {
+                success = true,
+                response = "File uploaded."
+            });
+        }
+    }
+}
---
+++ ProjectEDEO/ProjectEDEO/Migrations/201809262105464_cambio21.cs
@@ -0,0 +1,16 @@
+namespace Project_EDEO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class cambio21 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
