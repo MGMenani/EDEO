12b3805f3e8b3f0cc0f2ef34c4dd307e861caf12
Unknown <96jaslat@gmail.com>
2018-10-03 09:43:56 -0600

    Removed unnecessary images. Added the ability to send emails.

--- ProjectEDEO/ProjectEDEO/Controllers/AboutController.cs
+++ ProjectEDEO/ProjectEDEO/Controllers/AboutController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Project_EDEO.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Mail;
 
 namespace Project_EDEO.Controllers
 {
@@ -56,6 +58,31 @@ namespace Project_EDEO.Controllers
                 success = true,
                 response = "File uploaded."
             });
+        }
+
+        // POST: About/Email
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Email([Bind(Include = "Name,Email,Subject,Message")] Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage("96jaslat@gmail.com", "96jaslat@gmail.com");
+                SmtpClient client = new SmtpClient
+                {
+                    Port = 587,
+                    Host = "smtp.gmail.com",
+                    Credentials = new System.Net.NetworkCredential("96jaslat@gmail.com", "CONTRASEÑAMIEDO"),
+                    EnableSsl = true
+                };
+                mail.Subject = "[EDEO] - " + contact.Subject;
+                mail.Body = "From " + contact.Name + "(" + contact.Email + ")" + "\n" + contact.Message;
+                client.Send(mail);
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
     }
 }
---
+++ ProjectEDEO/ProjectEDEO/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_EDEO.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
